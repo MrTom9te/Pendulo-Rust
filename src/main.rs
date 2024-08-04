1333d31d71d004944e4ee63b411b4569560077af
MrTom9te <gabriel_santarem@outlook.com>
2024-08-04 15:00:20 -0400

    feature: Multi pendulum

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
-use speedy2d::{color::Color, window::WindowHandler, Graphics2D, Window};
-use std::f32;
+
+
+use speedy2d::{
+    color::Color,
+    font::{Font, TextLayout, TextOptions},
+    window::WindowHandler,
+    Graphics2D, Window,
+};
+use std::{f32, io::stdin};
 
 use vector::Vector;
 
 fn main() {
-    let window = Window::new_centered("Pendulum", (1000, 600)).unwrap();
-    let my_window_handler = MyWindowHandler {
-        p: (
-            Pendulum::new(500f32, 0f32, 100f32),
-            Pendulum::new(300f32, 0f32, 100f32),
-        ),
+    let mut quantity = String::new();
+    println!("Quantos pendulos?");
+    stdin().read_line(&mut quantity).unwrap();
+    let quantity: f32 = match quantity.trim().parse() {
+        Ok(numero) => numero,
+        Err(_) => {
+            println!("Erro: quantidade setada para 1 ");
+            1f32
+        }
     };
+
+    let window = Window::new_centered("Pendulum", (1000, 600)).unwrap();
+    let my_window_handler = |x: f32| -> MyWindowHandler {
+        let mut wh = MyWindowHandler { pendulos: vec![] };
+        let counter = 900f32 / x;
+
+        for i in 0..x as i32 {
+            wh.pendulos.push(Pendulum::new(
+                counter + ((i as f32) * counter),
+                0f32,
+                200f32,
+            ))
+        }
+
+        wh
+    };
+
     let win = my_window_handler;
-    window.run_loop(win);
+    window.run_loop(win(quantity));
 }
 struct MyWindowHandler {
-    p: (Pendulum, Pendulum),
+    pendulos: Vec<Pendulum>,
 }
 
 impl WindowHandler for MyWindowHandler {
@@ -24,16 +51,37 @@ impl WindowHandler for MyWindowHandler {
         helper: &mut speedy2d::window::WindowHelper<()>,
         graphics: &mut Graphics2D,
     ) {
-        graphics.clear_screen(Color::from_rgb(0.8, 10.0, 0.0));
-        self.p.0.update();
-        self.p.1.update();
-        self.p.0.draaw(graphics);
-        self.p.1.draaw(graphics);
+        graphics.clear_screen(Color::WHITE);
+        let bytes = include_bytes!("assets/fonts/noto-sans/NotoSans-Regular.ttf");
+        let font = Font::new(bytes).unwrap();
+        let data1 = &self.pendulos[1].angular_aceleration.to_string();
+        println!("{:.2}",&data1);
+
+        let block = font.layout_text(data1, 19f32, TextOptions::new());
+
+        for i in &mut self.pendulos {
+            i.draaw(graphics);
+            i.update();
+        }
+
+        //self.pendulos[0].draaw(graphics);
+        //self.pendulos[1].update();
+        //self.pendulos[0].draaw(graphics);
+        //self.pendulos[1].draaw(graphics);
+
+        for i in &mut self.pendulos {
+            graphics.draw_text(
+                (i.position.x - 30f32, i.position.y + 30f32),
+                Color::BLACK,
+                &block,
+            );
+        }
 
         helper.request_redraw();
     }
 }
 
+#[derive(Debug)]
 struct Pendulum {
     origim: vector::Vector,
     position: vector::Vector,
@@ -57,12 +105,12 @@ impl Pendulum {
             angular_velocity: 0.0,
             angular_aceleration: 0.0,
             r: r,
-            m: 1.0,
+            m: 0.5,
             g: 0.5,
         }
     }
     fn update(&mut self) {
-        self.angular_aceleration += -2f32 * self.angle.sin() * self.g / self.r;
+        self.angular_aceleration += -0.5f32 * self.angle.sin() * self.g / self.r;
 
         self.angular_velocity += self.angular_aceleration;
 
@@ -72,12 +120,6 @@ impl Pendulum {
             .set(self.r * self.angle.sin(), self.r * self.angle.cos());
 
         self.position.add(&self.origim);
-
-        print!(
-            "{:?}\r{:?}\r",
-            dbg!(&self.position),
-            dbg!(self.angular_aceleration)
-        );
     }
 
     fn draaw(&self, graphics: &mut Graphics2D) {
@@ -85,10 +127,10 @@ impl Pendulum {
             (self.origim.x, self.origim.y),
             (self.position.x, self.position.y),
             3.0,
-            Color::RED,
+            Color::BLACK,
         );
 
-        graphics.draw_circle((self.position.x, self.position.y), 30.0, Color::BLUE);
+        graphics.draw_circle((self.position.x, self.position.y), 30.0, Color::BLACK);
     }
 }
 
